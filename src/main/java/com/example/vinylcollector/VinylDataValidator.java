0bbc12d722189167721412697b46a74f3b187e53
Lorenz Bindernagel <lobinagel@outlook.de>
2023-12-11 16:52:18 +0100

    Added VinylDataValidatorTest.java

--- src/main/java/com/example/vinylcollector/VinylDataValidator.java
+++ src/main/java/com/example/vinylcollector/VinylDataValidator.java
@@ -8,7 +8,12 @@ import java.time.Year;
  * Every function throws an exception if invalid and returns true if valid.
  */
 public class VinylDataValidator {
-
+    /**
+     * Checks if release year is smaller or equal than current year and is not below zero
+     * @param year User input to validate
+     * @return true if validation was successful
+     * @throws InvalidInputException when release year doesn't meet the requirements
+     */
     public static boolean checkReleaseYear(int year) throws InvalidInputException{
 
     if(year > Year.now().getValue() || year < 0)
@@ -17,6 +22,12 @@ public class VinylDataValidator {
         return true;
     }
 
+    /**
+     * Checks if genre contains numbers
+     * @param genre User input to validate
+     * @return true if validation was successful
+     * @throws InvalidInputException when one or more numbers are found
+     */
     public static boolean checkGenre(String genre) throws InvalidInputException{
         if(genre.matches(".*\\d.*"))
             throw new InvalidInputException("Genre isn't allowed to contain numbers.");
---
+++ src/test/java/com/example/vinylcollector/VinylDataValidatorTest.java
@@ -0,0 +1,44 @@
+package com.example.vinylcollector;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Lorenz
+ * Includes test cases for VinylDataValidator class
+ */
+public class VinylDataValidatorTest {
+
+    /**
+     * Test case for checkReleaseYear method
+     */
+    @Test
+    public void testCheckReleaseYear() throws InvalidInputException{
+        // Test valid input
+        int year1 = 2020;
+        assertTrue(VinylDataValidator.checkReleaseYear(year1));
+
+        // Test invalid input: year greater than current year
+        int year2 = 2024;
+        assertThrows(InvalidInputException.class, () -> VinylDataValidator.checkReleaseYear(year2));
+
+        // Test invalid input: year less than zero
+        int year3 = -10;
+        assertThrows(InvalidInputException.class, () -> VinylDataValidator.checkReleaseYear(year3));
+    }
+
+    /**
+     * Test case for checkGenre method
+     */
+    @Test
+    public void testCheckGenre() throws InvalidInputException{
+        // Test valid input
+        String genre1 = "Rock";
+        assertTrue(VinylDataValidator.checkGenre(genre1));
+
+        // Test invalid input: genre contains numbers
+        String genre2 = "Pop2";
+        assertThrows(InvalidInputException.class, () -> VinylDataValidator.checkGenre(genre2));
+    }
+}
